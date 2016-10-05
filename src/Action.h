6e32fd52fa4b60a5516afd29c2dd051cbc691210
Victor Drouin Viallard <victor@drouin.io>
2016-10-04 20:09:40 -0400

    Ajout de timers/coût différents par action

--- src/Action.h
+++ src/Action.h
@@ -1,17 +1,19 @@
 #ifndef ACTION_H
 #define ACTION_H
 
+class Vacuum;
+
 /**
  * All possible types for Action.type
  */
 enum ActionType {
-    GoNorth,
-    GoSouth,
-    GoEast,
-    GoWest,
-    Suck,
-    Gather,
-    Iddle
+    GoNorth, // timer = 1, energy = 1
+    GoSouth, // idem
+    GoEast,  // idem
+    GoWest,  // idem
+    Suck,    // timer = t, energy = t, remove = 1.5 * t
+    Gather,  // timer = 3, energy = 1.4, remove = 1
+    Iddle    // sur base : timer = t, energy = 2 * t
 };
 
 struct Action {
